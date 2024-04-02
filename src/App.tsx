29e65c80043745b5b7280bb56b3bd17c2b6a1542
Harsh Karan <karanharshe577@gmail.com>
2024-04-03 00:47:14 +0530

    Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<NavBar />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
-    // <div>
-    //   <NavBar />
-    //   <div>
-
-    //   </div>
-    // </div>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import Social from "./Social";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="flex h-screen  justify-center ">
+        <div className="my-96 pl-16 py-8 ">
+          <Social />
+        </div>
+        <div className="flex flex-col items-center  mx-auto mt-48">
+          <div className="text-2xl ">Hi! My name is </div>
+          <div className="text-8xl font-bold py-6">Harsh Karan</div>
+          <div className="text-2xl">i am a Software Developer </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
